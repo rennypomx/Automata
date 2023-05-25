d6ec9ca24563aa4b92fda195b93197dc2286dd3d
Rennypoma <rennypoma684@gamil.com>
2023-05-25 15:55:46 -0500

    Solucion

--- src/javaapplication1/JavaApplication1.java
+++ src/javaapplication1/JavaApplication1.java
@@ -16,20 +16,88 @@ public class JavaApplication1 {
         static int estado = 0;
 
     
-    public static String muestraContenido(String archivo) throws FileNotFoundException, IOException { 
-    	String cadena; 
-        FileReader f = new FileReader(archivo); 
-        BufferedReader b = new BufferedReader(f); 
-        while((cadena = b.readLine())!=null) { 
-        	System.out.println(cadena); 
-                return(cadena);
-        } 
-        b.close(); 
-        return(null);
-	} 
-    
-    
-   /* public static void main(String[] args) throws IOException {
+  
+    public static void main(String[] args) throws IOException {
+        String codigo = muestraContenido("C:\\Users\\renny\\OneDrive\\Documentos\\datos.txt");
+        char[] aCaracteres = codigo.toCharArray();
+        System.out.println(aCaracteres.length);
+        int estado = 0;
+        for (char aCaractere : aCaracteres) {
+            System.out.println(aCaractere);
+            switch (estado) {
+                case 0:
+                    if (Character.isLetter(aCaractere)) {
+                        estado = 1;
+                        System.out.println("Es letra");
+                    } else if (Character.isDigit(aCaractere)) {
+                        estado = 2;
+                        System.out.println("Es digito");
+                    } else if (aCaractere == '{' || aCaractere == '}') {
+                        estado = 3;
+                        System.out.println("Es llave");
+                    } else if (aCaractere == '(' || aCaractere == ')') {
+                        estado = 4;
+                        System.out.println("Es paréntesis");
+                    } else {
+                        System.out.println("Caracter no reconocido");
+                        estado = 0;
+                    }
+                    break;
+                case 1:
+                    if (Character.isLetter(aCaractere)) {
+                        estado = 1;
+                        System.out.println("Es letra");
+                    } else {
+                        System.out.println("");
+                        estado = 0;
+                    }
+                    break;
+                case 2:
+                    if (Character.isDigit(aCaractere)) {
+                        estado = 2;
+                        System.out.println("Es digito");
+                    } else {
+                        System.out.println("");
+                        estado = 0;
+                    }
+                    break;
+                case 3:
+                    if (aCaractere == '{' || aCaractere == '}') {
+                        estado = 3;
+                        System.out.println("Es llave");
+                    } else {
+                        System.out.println("");
+                        estado = 0;
+                    }
+                    break;
+                case 4:
+                    if (aCaractere == '(' || aCaractere == ')') {
+                        estado = 4;
+                        System.out.println("Es parentesis");
+                    } else {
+                        System.out.println("");
+                        estado = 0;
+                    }
+                    break;
+                default:
+                    throw new AssertionError();
+            }
+        }
+    }
+
+    private static String muestraContenido(String archivo) throws IOException {
+        StringBuilder contenido = new StringBuilder();
+        try (BufferedReader br = new BufferedReader(new FileReader(archivo))) {
+            String linea;
+            while ((linea = br.readLine()) != null) {
+                contenido.append(linea);
+            }
+        }
+        return contenido.toString();
+    }
+}
+
+ /* public static void main(String[] args) throws IOException {
     	String codigo = muestraContenido("C:\\Users\\renny\\OneDrive\\Documentos\\datos.txt"); 
         char[] aCaracteres = codigo.toCharArray();
         System.out.println(aCaracteres.length);
@@ -72,61 +140,3 @@ public class JavaApplication1 {
             }
             
         }*/
-public static void main(String[] args) throws IOException {
-    String codigo = muestraContenido("C:\\Users\\renny\\OneDrive\\Documentos\\datos.txt"); 
-    char[] aCaracteres = codigo.toCharArray();
-    System.out.println(aCaracteres.length);
-    int estado = 0;
-    for (char aCaractere : aCaracteres) {
-        System.out.println(aCaractere);
-        switch (estado) {
-            case 0:
-                if(Character.isLetter(aCaractere)){
-                    estado = 1;
-                    System.out.println("Es letra");
-                }else if(Character.isDigit(aCaractere)){
-                    estado = 2;
-                    System.out.println("Es digito");
-                }else if(aCaractere == '{' || aCaractere == '}'){
-                    estado = 3;
-                    System.out.println("Es llave");
-                }else{
-                    System.out.println("Caracter no reconocido");
-                    estado = 0;
-                }
-                break;
-            case 1:
-                if(Character.isLetter(aCaractere)){
-                    estado = 1;
-                    System.out.println("Es letra");
-                }else{
-                    System.out.println("Fin de secuencia de letras");
-                    estado = 0;
-                }
-                break;
-            case 2:
-                if(Character.isDigit(aCaractere)){
-                    estado = 2;
-                    System.out.println("Es digito");
-                }else{
-                    System.out.println("Fin de secuencia de digitos");
-                    estado = 0;
-                }
-                break;
-            case 3:
-                if(aCaractere == '{' || aCaractere == '}'){
-                    estado = 3;
-                    System.out.println("Es llave");
-                }else{
-                    System.out.println("Fin de secuencia de llaves");
-                    estado = 0;
-                }
-                break;
-            default:
-                throw new AssertionError();
-        }
-    }
-    }
-    
-}
-    
